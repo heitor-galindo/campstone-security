c49541875b890a312184bd54ff6a6ff49b3c4e5b
Heitor A. Galindo <heitor.augusto.galindo@live.com>
2026-01-19 12:50:48 -0300

    jwt bug

--- ReservationMS/src/main/java/com/skistation/reservationms/clients/StudentClient.java
+++ ReservationMS/src/main/java/com/skistation/reservationms/clients/StudentClient.java
@@ -3,11 +3,13 @@ package com.skistation.reservationms.clients;
 import com.skistation.reservationms.configuration.FeignClientConfigStudent;
 import com.skistation.reservationms.dto.StudentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "STUDENTMS", configuration = FeignClientConfigStudent.class)
+@FeignClient(
+    name = "STUDENTMS",
+    configuration = FeignClientConfigStudent.class,
+    url = "http://campstone-security-student-ms:8084")
 public interface StudentClient {
 
   @GetMapping("/students/{id}")
